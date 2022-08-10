e5cecd022083ee056a78fafc14c786d8636a8718
Suleman Ahmad <87057938+Sulman23@users.noreply.github.com>
2022-08-10 12:04:04 +0500

    sidebar styling almost done

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,70 +1,89 @@
 import React from "react";
+// import Link
+import { Link } from "react-router-dom";
 // icons
 import { AiOutlineBarChart } from "react-icons/ai";
 import { FaTachometerAlt } from "react-icons/fa";
 import { FaRegFileAlt } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaUserFriends } from "react-icons/fa";
-import { BsArrowDownShort } from "react-icons/bs";
+import { FaAngleDown } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <>
-      <div id="sidebar" className="bg-dark">
-        <nav>
-          <ul className="nav flex-column">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="">
-                <FaTachometerAlt />
-                Dashboard
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                href="#submenuProducts"
-                data-toggle="collapse"
-                data-target="#submenuProducts"
-              >
-                <FaShoppingCart /> Products
-                <BsArrowDownShort />
-              </a>
-              <ul
-                class="list-unstyled flex-column pl-3 collapse"
-                id="submenuProducts"
-                aria-expanded="false"
-              >
-                <li class="nav-item mb-2 ">
-                  <a class="nav-link text-secondary" href="">
-                    <FaShoppingCart /> Add Products
-                  </a>
-                </li>
-                <li class="nav-item mb-2 ">
-                  <a class="nav-link text-secondary" href="">
-                    <FaShoppingCart /> All Products
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <FaRegFileAlt /> Orders
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+    <div id="sidebar" className="my-sidebar bg-light">
+      <nav>
+        <ul className="nav flex-column p-3">
+          <li className="nav-item mb-2">
+            <Link className="nav-link active" aria-current="page" to="/">
+              <FaTachometerAlt />
+              <span className="ms-2">Dashboard</span>
+            </Link>
+          </li>
+
+          <li className="nav-item mb-2">
+            <Link
+              className="nav-link"
+              to="#submenuProducts"
+              data-bs-toggle="collapse"
+              data-bs-target="#submenuProducts"
+            >
+              <FaShoppingCart />
+              <span className="mx-2">Products</span>
+              <FaAngleDown />
+            </Link>
+            <ul
+              class="list-unstyled flex-column pl-3 collapse"
+              id="submenuProducts"
+            >
+              <li class="nav-item ms-2 mb-2 ">
+                <Link class="nav-link text-secondary" to="">
+                  <FaShoppingCart /> Add Products
+                </Link>
+              </li>
+              <li class="nav-item ms-2 mb-2 ">
+                <Link class="nav-link text-secondary" to="">
+                  <FaShoppingCart /> All Products
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item mb-2">
+            <Link
+              className="nav-link"
+              to="#submenuOrders"
+              data-bs-toggle="collapse"
+              data-bs-target="#submenuOrders"
+            >
+              <FaRegFileAlt />
+              <span className="mx-2">Orders</span>
+              <FaAngleDown />
+            </Link>
+            <ul
+              class="list-unstyled flex-column pl-3 collapse"
+              id="submenuOrders"
+              aria-expanded="false"
+            >
+              <li class="nav-item ms-2 mb-2 ">
+                <Link class="nav-link text-secondary" to="">
+                  <FaRegFileAlt /> All Orders
+                </Link>
+              </li>
+            </ul>
+          </li>
+          {/* <li className="nav-item mb-2">
+              <Link className="nav-link" to="/">
                 <FaUserFriends /> Customers
-              </a>
+              </Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <AiOutlineBarChart /> Reports
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+            <li className="nav-item mb-2">
+              <Link className="nav-link" to="/">
+                <LinkiOutlineBarChart /> Reports
+              </Link>
+            </li> */}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
