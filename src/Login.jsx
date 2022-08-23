da4a567ab257c04b27a703fa4bd8eb8c0c831e12
Suleman Ahmad <87057938+Sulman23@users.noreply.github.com>
2022-08-23 11:28:00 +0500

    working on Login

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,33 +3,31 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // importing saveTransporter API
 import { saveTransporter } from "./services/modalService";
+import AddTransporter from "./transporters/AddTransporter";
 
 const Login = () => {
   const Navigate = useNavigate();
   // State for transporter
-  const [transporter, setTransporter] = useState({
-    full_name: "",
-    transporter_phone: "",
+  const [Login, setLogin] = useState({
     email: "",
-    address: "",
-    city: "",
+    password: "",
   });
   // handle on Change
   const handleChange = (e) => {
-    const Transporter = {
-      ...transporter,
+    const Login = {
+      ...Login,
       [e.target.name]: e.target.value,
     };
-    setTransporter(Transporter);
+    setLogin(Login);
   };
 
   // handle on Submit
-  const handleSubmit = async (e, transporter) => {
+  const handleSubmit = async (e, Login) => {
     e.preventDefault();
-    console.log(transporter);
-    await saveTransporter(transporter);
+    console.log(Login);
+    await saveLogin(Login);
     console.log("transpoter added");
-    // Navigate("/transporters/all");
+    // Navigate("/Logins/all");
   };
   return (
     <div className="container-fluid">
@@ -67,93 +65,8 @@ const Login = () => {
           </form>
         </div>
         <div className="col-md-6 bg-white p-4">
-          <form onSubmit="">
-            <h1>Register transporter</h1>
-            <p className="text-medium-emphasis">Add your transporter details</p>
-            <div className="mb-3">
-              <label htmlFor="fullName" className="form-label">
-                Full Name
-              </label>
-              <input
-                className="form-control"
-                type="text"
-                id="fullName"
-                placeholder="Bilal Transports"
-                required
-                name="full_name"
-                onChange={handleChange}
-                value={transporter.full_name}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="phone" className="form-label">
-                Phone
-              </label>
-              <input
-                className="form-control"
-                type="text"
-                id="phone"
-                placeholder="03211234567"
-                required
-                name="transporter_phone"
-                onChange={handleChange}
-                value={transporter.transporter_phone}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                className="form-control"
-                type="email"
-                id="email"
-                placeholder="email@email.com"
-                required
-                name="email"
-                onChange={handleChange}
-                value={transporter.email}
-              />
-            </div>
-            <div className="form-group mb-3">
-              <label htmlFor="address" className="form-label">
-                Address
-              </label>
-              <textarea
-                className="form-control"
-                id="address"
-                rows="3"
-                required
-                name="address"
-                onChange={handleChange}
-                value={transporter.address}
-              ></textarea>
-            </div>
-            <div className="form-group mb-3">
-              <label htmlFor="city">City</label>
-              <select
-                className="form-control"
-                id="city"
-                required
-                name="city"
-                onChange={handleChange}
-                value={transporter.city}
-              >
-                <option>Chenab Nagar</option>
-                <option>Lahore</option>
-                <option>Karachi</option>
-                <option>Islamabad</option>
-                <option>Faisalabad</option>
-              </select>
-            </div>
-            <button
-              type="sumbmit"
-              className="btn btn-primary mb-3"
-              onClick={(e) => handleSubmit(e, transporter)}
-            >
-              Register
-            </button>
-          </form>
+          {/* Register Transporter */}
+          <AddTransporter />
         </div>
       </div>
     </div>
